99628917d7a21aea15006e5ecf4515ecb33ba4cc
Adawat ( sonata_mini ) <prophecyx@hotmail.com>
2015-12-13 02:17:31 +0700

    Add Xcode project

---
+++ Build/Xcode/Pillar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pillar
+//
+//  Created by Adawat Chanchua on 12/13/2558 BE.
+//  Copyright © 2558 Adawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
